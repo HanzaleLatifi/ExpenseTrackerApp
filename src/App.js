6f27937149165cc63c83cc84559c7eeee6f37a53
h_latifi8 <h.latifi03@gmail.com>
2021-08-29 07:38:29 +0430

    start-Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ExpenseApp from './components/ExpenseApp';
+
+function App() {
+  return (
+    <>
+      <ExpenseApp/>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseApp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExpenseApp() {
+    return (
+        <section>
+            
+        </section>
+    )
+}
+
+export default ExpenseApp
